ac6039467e85620665ae23c35f22fe2431300de9
Anastasiya Antonova <nastyaantonovaa@gmail.com>
2026-02-09 23:52:53 +0300

    add moveIngredientTest()

--- src/test/java/BurgerTest.java
+++ src/test/java/BurgerTest.java
@@ -1,8 +1,11 @@
 import org.junit.Test;
 import praktikum.Bun;
 import praktikum.Burger;
+import praktikum.Ingredient;
+import praktikum.IngredientType;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class BurgerTest {
 
@@ -12,6 +15,27 @@ public class BurgerTest {
         Bun bun = new Bun("black bun", 100);
         burger.setBuns(bun);
         assertEquals("Другая булочка", bun, burger.bun);
+    }
+
+    @Test
+    public void addIngredientTest() {
+        Burger burger = new Burger();
+        Ingredient ingredient = new Ingredient(IngredientType.SAUCE,"sour cream", 200);
+        burger.addIngredient(ingredient);
+        assertTrue("Ингредиент не добавился", burger.ingredients.contains(ingredient));
+    }
+
+    @Test
+    public void moveIngredientTest() {
+        Burger burger = new Burger();
+        Ingredient ingredientSauce = new Ingredient(IngredientType.SAUCE, "chili sauce", 300);
+        Ingredient ingredientFirstFilling = new Ingredient(IngredientType.FILLING, "cutlet", 100);
+        Ingredient ingredientSecondFilling = new Ingredient(IngredientType.FILLING, "sausage", 300);
+        burger.addIngredient(ingredientSauce);
+        burger.addIngredient(ingredientFirstFilling);
+        burger.addIngredient(ingredientSecondFilling);
+        burger.moveIngredient(0, 2);
+        assertEquals("Перенос ингредиентов неверный", ingredientSauce, burger.ingredients.get(2));
     }
 }
 
